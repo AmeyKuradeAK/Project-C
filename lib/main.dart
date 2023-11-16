2349b673388790e09b3148371e5b778efbf41a20
AmeyKuradeAK <ameykurade60@gmail.com>
2023-11-16 21:44:06 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+    return const Placeholder();
   }
 }
