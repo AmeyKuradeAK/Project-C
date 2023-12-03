24a0755dd72419597e5eaa90559b41ab2bbbf447
AmeyKuradeAK <ameykurade60@gmail.com>
2023-12-03 15:03:49 +0530

    Added Posts and minor bugs fixed

--- lib/Pages/home_page/homepage.dart
+++ lib/Pages/home_page/homepage.dart
@@ -18,10 +18,133 @@ class mainHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: _post.length,
-        itemBuilder: (context, index) {
-          return HomeList(child: _post[index]);
-        });
+    return ListView(
+      scrollDirection: Axis.vertical,
+      children: [
+        //1st Post
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Container(
+              //Giving the Curve the Posts
+              height: 300,
+              color: Colors.blue,
+              child: ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      "assets/thumbnail/Intro_to_flutter.png",
+                      height: 200,
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      "Introduction to Flutter",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                ],
+              )),
+        ),
+
+        //2nd Post
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Container(
+              height: 300,
+              color: Colors.blue,
+              child: ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      "assets/thumbnail/Intro_to_python.jpg",
+                      height: 200,
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      "Introduction to Python",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                ],
+              )),
+        ),
+
+        //3rd Post
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Container(
+              height: 300,
+              color: Colors.blue,
+              child: ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      "assets/thumbnail/Intro_to_rust.png",
+                      height: 200,
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      "Introduction to rust",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                ],
+              )),
+        ),
+
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Container(
+              height: 300,
+              color: Colors.blue,
+              child: ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      "assets/thumbnail/Intro_to_Ruby.jpg",
+                      height: 200,
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      "Introduction to Ruby",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                ],
+              )),
+        ),
+
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Container(
+              height: 300,
+              color: Colors.blue,
+              child: ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      "assets/thumbnail/Intro_to_java.png",
+                      height: 200,
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      "Introduction to java",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                ],
+              )),
+        )
+      ],
+    );
   }
 }
