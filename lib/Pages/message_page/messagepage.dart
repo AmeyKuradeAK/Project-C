2fef60369292491b5f6771dd75dcc29ddd7dadcb
AmeyKuradeAK <ameykurade60@gmail.com>
2023-12-04 19:25:06 +0530

    Change Logs added in message page

--- lib/Pages/message_page/messagepage.dart
+++ lib/Pages/message_page/messagepage.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MessagePage extends StatelessWidget {
   const MessagePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Message Page",
-        style: TextStyle(fontSize: 20),
+    return Center(
+      child: Container(
+        child: InkWell(
+          onTap: () => launchUrl(Uri.parse('https://github.com/AmeyKuradeAK/Project-C')),
+          child: Text("Change Logs"),
+        ),
       ),
     );
   }
